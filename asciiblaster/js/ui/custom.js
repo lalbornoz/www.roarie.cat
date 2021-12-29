3930fdcd8110c96c36cc7bda65046ca69726dc92
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2021-12-29 20:09:53 +0100

    asciiblaster/js/ui/custom.js: correctly calculate size of selected region on add.

--- asciiblaster/js/ui/custom.js
+++ asciiblaster/js/ui/custom.js
@@ -9,7 +9,7 @@ var custom = (function(){
       new_brush = brush.clone()
     } else {
       if (!(selection.a[0] === 0 && selection.a[1] === 0 && selection.b[0] === 0 && selection.b[1] === 0)) {
-        new_brush = new Matrix(selection.b[0] - selection.a[0], selection.b[1] - selection.a[1], function(x,y){
+        new_brush = new Matrix((selection.b[0] + 1) - selection.a[0], (selection.b[1] + 1) - selection.a[1], function(x,y){
           var cell = canvas.getCell(x + selection.a[0], y + selection.a[1]).clone()
           if (cell.fg === 99 && cell.bg === 99 && cell.opacity === 0) {
             cell.fg = 0; cell.bg = 1

0d986ca4f6633981a95cfa1a30d17f3b5444d92c
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2022-01-05 14:03:37 +0100

    asciiblaster/js/app.js: implement localStorage'd resizable UI elements. asciiblaster/css/sally.css:.custom: remove float: left property. asciiblaster/doc/index.html: updated.

--- asciiblaster/js/app.js
+++ asciiblaster/js/app.js
@@ -17,8 +17,14 @@ var mouse = { x: 0, y: 0 }
 
 var fd_currentDiv = null;
 var fd_divOrigPosition = {};
-var fd_isDown = false;
+var fd_divOrigSize = {};
+var fd_isLeftDown = false;
+var fd_isRightDown = false;
 var fd_mousePosition = {};
+
+var fd_original_mouse = [0, 0];
+var fd_original_pos = [0, 0];
+var fd_original_size = [0, 0];
 
 // {{{ function initFloatableDivs()
 function initFloatableDivs() {
@@ -42,6 +48,7 @@ function initFloatableDivs() {
     div.style.border = "1px solid grey";
     div.style.margin = "4px";
     fd_divOrigPosition[divName] = [div.style.left, div.style.top];
+    fd_divOrigSize[divName] = [div.offsetWidth, div.offsetHeight];
 
     if (divSetting === null) {
       div.style.position = "relative";
@@ -49,10 +56,19 @@ function initFloatableDivs() {
       div.style.left = localStorage.getItem("floatableDivs." + divName + ".left")
       div.style.position = localStorage.getItem("floatableDivs." + divName + ".position")
       div.style.top = localStorage.getItem("floatableDivs." + divName + ".top")
-    }
+
+      if (div.id !== "canvas_wrapper") {
+        if (localStorage.getItem("floatableDivs." + divName + ".width") !== null) {
+          div.style.width = localStorage.getItem("floatableDivs." + divName + ".width");
+        };
+        if (localStorage.getItem("floatableDivs." + divName + ".height") !== null) {
+          div.style.height = localStorage.getItem("floatableDivs." + divName + ".height");
+        };
+      };
+    };
 
     div.addEventListener("mousedown", function(e) {
-      if (e.ctrlKey && e.altKey) {
+      if ((e.button === 0) && e.ctrlKey && e.altKey) {
         let div = e.currentTarget;
 
         if (e.shiftKey) {
@@ -63,36 +79,89 @@ function initFloatableDivs() {
           localStorage.setItem("floatableDivs." + div.id + ".top", fd_divOrigPosition[div.id][1]);
           div.style.top = fd_divOrigPosition[divName][1];
         } else {
-          fd_currentDiv = div; fd_isDown = true;
+          fd_currentDiv = div; fd_isLeftDown = true;
           offset = [div.offsetLeft - e.clientX, div.offsetTop - e.clientY];
           div.setAttribute("data-offset", JSON.stringify(offset));
+        };
+      } else if ((e.button === 2) && e.ctrlKey) {
+        let div = e.currentTarget;
+
+        if (div.id !== "canvas_wrapper") {
+          if (e.altKey) {
+            localStorage.setItem("floatableDivs." + div.id + ".width", fd_divOrigSize[div.id][0] + "px");
+            div.style.width = fd_divOrigSize[div.id][0] + "px";
+            localStorage.setItem("floatableDivs." + div.id + ".height", fd_divOrigSize[div.id][1] + "px");
+            div.style.height = fd_divOrigSize[div.id][1] + "px";
+          } else {
+            fd_currentDiv = div; fd_isRightDown = true;
+
+            fd_original_size[0] = parseFloat(getComputedStyle(div, null).getPropertyValue('width').replace('px', ''));
+            fd_original_size[1] = parseFloat(getComputedStyle(div, null).getPropertyValue('height').replace('px', ''));
+            fd_original_pos[0] = div.getBoundingClientRect().left;
+            fd_original_pos[1] = div.getBoundingClientRect().top;
+            fd_original_mouse[0] = e.pageX;
+            fd_original_mouse[1] = e.pageY;
+          };
         };
       };
     }, true);
 
     document.addEventListener("mouseup", function(e) {
-      fd_isDown = false;
+      e.preventDefault();
+      if (fd_isLeftDown && (e.button === 0)) {
+        e.preventDefault(); fd_isLeftDown = false;
+      } else if (fd_isRightDown && (e.button === 2)) {
+        e.preventDefault(); fd_isRightDown = false;
+      };
+    }, true);
+
+    document.addEventListener("contextmenu", function(e) {
+      e.preventDefault();
     }, true);
 
     document.addEventListener("mousemove", function(e) {
-      if (fd_isDown && fd_currentDiv) {
-        e.preventDefault();
+      if (fd_currentDiv) {
+        if (fd_isLeftDown) {
+          e.preventDefault();
 
-        fd_mousePosition = {x: event.clientX, y: event.clientY};
-        const offset = JSON.parse(fd_currentDiv.getAttribute("data-offset"));
+          fd_mousePosition = {x: event.clientX, y: event.clientY};
+          const offset = JSON.parse(fd_currentDiv.getAttribute("data-offset"));
 
-        localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".left", (fd_mousePosition.x + offset[0]) + "px");
-        fd_currentDiv.style.left = (fd_mousePosition.x + offset[0]) + "px";
-        localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".position", "absolute");
-        fd_currentDiv.style.position = "absolute";
-        localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".top", (fd_mousePosition.y + offset[1]) + "px");
-        fd_currentDiv.style.top = (fd_mousePosition.y + offset[1]) + "px";
-        localStorage.setItem("floatableDivs." + fd_currentDiv.id, true);
+          localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".left", (fd_mousePosition.x + offset[0]) + "px");
+          fd_currentDiv.style.left = (fd_mousePosition.x + offset[0]) + "px";
+          localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".position", "absolute");
+          fd_currentDiv.style.position = "absolute";
+          localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".top", (fd_mousePosition.y + offset[1]) + "px");
+          fd_currentDiv.style.top = (fd_mousePosition.y + offset[1]) + "px";
+          localStorage.setItem("floatableDivs." + fd_currentDiv.id, true);
 
-        if (fd_currentDiv.id === "canvas_wrapper") {
-          selection.reposition()
-        }
-      }
+          if (fd_currentDiv.id === "canvas_wrapper") {
+            selection.reposition()
+          };
+        } else if (fd_isRightDown) {
+          e.preventDefault();
+
+          /*
+           * XXX
+           * https://medium.com/the-z/making-a-resizable-div-in-js-is-not-easy-as-you-think-bda19a1bc53d
+           */
+
+          const width = fd_original_size[0] + (e.pageX - fd_original_mouse[0]);
+          const height = fd_original_size[1] + (e.pageY - fd_original_mouse[1])
+          if (width > fd_divOrigSize[fd_currentDiv.id][0]) {
+            localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".width", width + "px");
+            fd_currentDiv.style.width = width + "px";
+          } else {
+            localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".width", fd_divOrigSize[fd_currentDiv.id][0] + "px");
+          }
+          if (height > fd_divOrigSize[fd_currentDiv.id][1]) {
+            localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".height", height + "px");
+            fd_currentDiv.style.height = height + "px";
+          } else {
+            localStorage.setItem("floatableDivs." + fd_currentDiv.id + ".height", fd_divOrigSize[fd_currentDiv.id][1] + "px");
+          }
+        };
+      };
     }, true);
   });
 }
